11aa888c821f0b83c46c84fb86c5bfde652f187b
Elco Jacobs <github@brewpi.com>
2016-02-12 16:33:39 +0100

    Initial commit of Django app for BrewPi. Nothing BrewPi specific yet. Just followed DRF quickstart and added swagger.

---
+++ brewpiweb/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from brewpiweb.rest import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
+]
